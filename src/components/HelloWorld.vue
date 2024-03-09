df64011e2f9f9b3a86cde8b767f5f5a2c5b52129
Jaiprakash siyyadri <jaiprakash.siyyadri@gramener.org>
2024-03-09 14:46:11 +0530

    upd: added test tex

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,10 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
+  <div>
+  From JP
+  </div>
+
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
